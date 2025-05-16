d0a279f431d26f25fced957691c16c00779b3c36
dinel <rddinel@gmail.com>
2025-05-16 10:04:55 +0200

    Exercices partie 1

---
+++ Exercice 2/ex2.js
@@ -0,0 +1,42 @@
+// Indiquer les valeurs affichées par les instructions suivantes :
+
+// ==========================
+// Niveau 1
+// ==========================
+console.log(5 + 3);         
+console.log(10 - 4);       
+console.log(2 * 3);           
+console.log(8 / 2);          
+console.log(9 % 4);            
+console.log("Hello" + " World");
+console.log(5 == "5");         
+console.log(5 === "5");       
+console.log(true && false);     
+console.log(true || false);    
+
+// ==========================
+// Niveau 2
+// ==========================
+let a = 5;
+a += 2;
+console.log(a);                 
+
+console.log(!true);           
+console.log(3 > 2 && 4 < 5);   
+console.log(3 > 2 && 4 > 5);    
+console.log(3 > 2 || 4 > 5);   
+console.log(false || (true && false)); 
+
+// ==========================
+// Niveau 3
+// ==========================
+console.log("5" + 3);           
+console.log("5" - 3);        
+console.log("5" * "2");         
+console.log("a" * 2);        
+console.log(undefined == null); 
+console.log(undefined === null);
+console.log(0 == false);        
+console.log(0 === false);     
+console.log("" || "JS");     
+console.log("JS" && "Fun");    
---
+++ Exercice 3/ex3.js
@@ -0,0 +1,34 @@
+// Fonction de gestion de majorité (console.log)
+// paramètre : age
+// age superieur  : "Vous êtes majeur"
+// age égal à 18 : "Vous êtes tout juste majeur"
+// age inferieur à 18 : "Vous êtes mineur"
+function checkMajority(age){}
+
+// Fonction qui renvoie le nom du jour selon un numero fourni en paramètre
+// exemple : 1 -> "Lundi"
+function getDayName(number){}
+
+// Fonction qui affiche les nombres de 1 à 10 avec une boucle for 
+function displayNumbers(){}
+
+// Fonction qui compte de 5 à 1 avec une boucle while
+function countDown(){}
+
+// Fonction jeu : l'utilsateur doit deviner un nombre secret entre 1 et 10 
+// While et if / else
+function guessNumber(secretNumber){}
+
+// Affiche les nombres de 1 à 10 et indique s'ils sont pairs ou impairs
+function showEvenOdd(){}
+
+// Fonction qui simule une connexion 
+// l'utilisateur à 3 tentatives pour deviner un mot de passe
+function login(password){}
+
+// Fonction qui vérifie si un nombre est parfait 
+// un nombre parfait est un nombre qui est égal à la somme de ses diviseurs
+// Exemple : 6 est un nombre parfait car 6 = 1 + 2 + 3
+function isPerfectNumber(number){}
+
+
