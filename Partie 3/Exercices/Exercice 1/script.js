230397d97782e2951bcc34a6f4cc98bd54f35c31
AxelLaisney <axel.laisney@hotmail.com>
2025-05-19 15:44:02 +0200

    Part 3 exercices

--- Partie 3/Exercices/Exercice 1/script.js
+++ Partie 3/Exercices/Exercice 1/script.js
@@ -5,3 +5,84 @@
 
 // Dans un second temps, créez trois instances de ces classes, et faites les intéragir en fonction des méthodes 
 // et attributs spécifiques de chaque personnage.
+class Warrior {
+    constructor(name, health, attack){
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+    }
+    attack(){
+        let n = Math.floor(Math.random() * (this.attack + 1))+10;
+        console.log("your attack deals "+n+" damages");
+    }
+    defend(){
+        let n = Math.floor(Math.random() * 16)+10;
+        console.log("you reduced the damages received by "+n) ;
+    }
+    item(){
+        let n = Math.floor(Math.random() * 6)+1;
+        console.log("you used an item for "+n+" damages") ;
+    }
+}
+
+class Fairy {
+    constructor(name, health, attack, mana){
+        this.name = name;
+        this.health = health;
+        this.attack = attack;  
+        this.mana = mana;
+    }
+    attack(){
+        let n = Math.floor(Math.random() * (this.attack + 1));
+        console.log("your attack deals "+n+" damages") ;
+    }
+    defend(){
+        let n = Math.floor(Math.random() * 16)+5;
+        console.log("you reduced the damages received by "+n) ;
+    }
+    heal(){
+        if(this.mana > 0){
+            this.mana =- 5;
+            let n = Math.floor(Math.random() * 6)+5;
+            console.log("You heal for "+n+" health") ;
+        }else{
+            console.log("not enough mana") ;
+        }
+
+    }
+}
+
+class Mage {
+    constructor(name, health, attack, mana){
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+        this.mana;
+    }
+    attack(){
+        let n = Math.floor(Math.random() * (this.attack + 1));
+        console.log("your attack deals "+n+" damages") ;
+    }
+    defend(){
+        let n = Math.floor(Math.random() * 5)+1;
+        console.log("you reduced the damages received by "+n) ;
+    }
+    fireBall(){
+        if(this.mana > 0){
+            this.mana =- 8;
+            let n = Math.floor(Math.random() * 21)+10;
+            console.log("you fireball deals "+n+" damages") ;
+        }else{
+            console.log("not enough mana");
+        }
+
+    }
+}
+
+let mage1 = new Mage("Red", 25, 5, 50);
+let warrior1 = new Warrior("Bob", 30, 15);
+let fairy1 = new Fairy("Elisa", 20, 3, 60);
+
+warrior1.attack();
+mage1.fireBall();
+fairy1.heal();
--- Partie 3/Exercices/Exercice 2/script.js
+++ Partie 3/Exercices/Exercice 2/script.js
@@ -2,3 +2,69 @@
 // Créez une classe mère "Personnage" avec des attributs et méthodes communes 
 // Reprenez puis corrigez les classes filles en utilisant l'héritage, et si nécessaire 
 // redéfinissez des comportements spécifiques aux méthodes 
+class Personnage{
+    constructor(name, health, attack, defense){
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+        this.defense = defense;
+    }
+    attack(){
+        let n = Math.floor(Math.random * (this.attack +1 ))+1;
+        console.log("Your attacks for "+n+" damages");
+    }
+    defend(){
+        let n = Math.floor(Math.random * (this.defense + 1 ))+5;
+        console.log("You defend for "+n+" damages");
+    }
+}
+
+class Warrior extends Personnage{
+    constructor(name){
+        super(name, 40, 15, 15);
+    }
+    item(){
+        let n = Math.floor(Math.random * 5)+1;
+        console.log("Your used an itema for "+n+" damages");
+    }
+}
+
+class Mage extends Personnage{
+    constructor(name){
+        super(name, 20, 5);
+        this.mana = 60;
+    }
+    fireball(){
+        if(mane > 0){
+            this.mana =- 8;
+            let n = Math.floor(Math.random * (16))+15;
+            console.log("Your fireball deals "+n+" damages");
+        }else{
+            console.log("not enough mana")
+        }
+    }
+}
+
+class Fairy extends Personnage{
+    constructor(name){
+        super(name, 15, 10);
+        this.mana = 50;
+    }
+    heal(){
+        if(mane > 0){
+            this.mana =- 8;
+            let n = Math.floor(Math.random * (11))+5;
+            console.log("You heal for "+n+" health");
+        }else{
+            console.log("not enough mana")
+        }
+    }
+}
+
+let warrior1 = new Warrior("Bob");
+let mage1 = new Mage("Carla");
+let fairy1 = new Fairy("starla");
+
+warrior1.attack;
+mage1.fireBall;
+fairy1.heal
