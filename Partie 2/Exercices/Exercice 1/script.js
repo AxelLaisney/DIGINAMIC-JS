673af34bc32c463350c692197d2fe70b83d640b5
AxelLaisney <axel.laisney@hotmail.com>
2025-05-19 11:39:23 +0200

    Exercices part 2

--- Partie 2/Exercices/Exercice 1/script.js
+++ Partie 2/Exercices/Exercice 1/script.js
@@ -4,24 +4,37 @@
 // Fonction 1 : Changer le texte du titre principal (h1)
 function changeTitle() {
     // TODO : Sélectionne l'élément avec l'id "main-title" et change son texte pour "Titre modifié !"
-
+      let title = document.getElementById("main-title");
+      title.textContent = "Titre modifé";
   }
   
   // Fonction 2 : Surligner tous les paragraphes en jaune
   function highlightParagraphs() {
     // TODO : Sélectionne tous les éléments avec la classe "description"
     // et change leur arrière-plan pour "yellow"
+      let ele = document.getElementsByClassName("description");
+      for(let i = 0; i < ele.length; i++){
+        ele[i].style.backgroundColor = "yellow";
+        ele[i].style.textDecoration = "underline"
+      }
   }
   
   // Fonction 3 : Ajouter un nouvel élément à la liste
   function addItem() {
     // TODO : Crée un nouvel élément <li> avec le texte "Nouvel Item"
     // puis l'ajoute à la fin de la liste avec l'id "items-list"
+    let list = document.getElementById("items-list");
+    let newLi = document.createElement("li");
+    newLi.textContent = "nouvel item";
+    newLi.classList.add("item");
+    list.appendChild(newLi);
   }
   
   // Fonction 4 : Supprimer le dernier élément de la liste
   function removeLastItem() {
     // TODO : Supprime le dernier élément <li> de la liste
+    let ele = document.getElementById("items-list");
+    ele.lastChild.remove();
   }
 
   // Fonction 5 : Cacher / afficher la liste (ul)
@@ -29,6 +42,12 @@ function toggleListVisibility() {
     const list = document.getElementById("items-list");
     // TODO : Si la liste est visible, on la cache. Sinon, on l'affiche.
     // Utilise style.display
+      let ele = document.getElementById("items-list");
+      if(ele.style.display == "none"){
+        ele.style.display = "block";
+      }else{
+        ele.style.display = "none";
+      }
   }
   
   // Fonction 6 : Changer le texte du 2e paragraphe
@@ -36,6 +55,7 @@ function toggleListVisibility() {
     const paragraphs = document.querySelectorAll(".description");
     if (paragraphs.length >= 2) {
       // TODO : Change le texte du 2e paragraphe pour "Paragraphe modifié dynamiquement !"
+      paragraphs[1].textContent = "paragraphe modifié dynamiquement"
     }
   }
   
--- Partie 2/Exercices/Exercice 2/script.js
+++ Partie 2/Exercices/Exercice 2/script.js
@@ -4,27 +4,60 @@
 function addArticle() {
     // TODO : Créer un nouvel élément <div> avec la classe "article" et le texte "Nouvel Article"
     // et l'ajouter dans #articles-container
+    let ele = document.getElementById("articles-container");
+    let div = document.createElement("div");
+    div.textContent = "Nouvel article";
+    div.classList.add("article");
+    ele.appendChild(div);
   }
   
   // Fonction 2 : Rechercher un article
   function searchArticle() {
     // TODO : Récupérer la valeur de l'input
     // Pour chaque .article, affiche-le uniquement si son texte contient la valeur entrée
+    let input = document.getElementById("search-input").value;
+    let articles = document.getElementById("articles-container");
+    if(input != ""){
+      for(let child of articles.children){
+        if(child.innerHTML != input){
+          child.style.display = "none";
+        }
+      }
+    }else{
+      let article = document.getElementsByClassName("article");
+      for(let i = 0; i < article.length; i++){
+        article[i].style.display = "block";
+      }
+    }
+  
   }
   
   // Fonction 3 : Changer la couleur de fond des articles
   function changeBackgroundColor() {
     // TODO : Récupérer la valeur sélectionnée dans le <select>
     // Appliquer cette couleur comme background à tous les éléments .article
+    let ele = document.getElementById("color-picker")
+    let articles = document.getElementsByClassName("article");
+    for(let i = 0; i < articles.length; i++){
+      articles[i].style.backgroundColor = ele.value;
+    }
   }
   
   // Fonction 4 : Afficher une alerte quand on survole un article
   function setupHoverAlert() {
+    alert("Hello");
   }
   
   // --- Événements  Ici ---
+document.getElementById("add-article").addEventListener("click", addArticle);
+document.getElementById("search-input").addEventListener("input", searchArticle);
+document.getElementById("color-picker").addEventListener("change", changeBackgroundColor);
+let articles = document.getElementsByClassName("article");
+for (let i = 0; i < articles.length; i++){
+  articles[i].addEventListener("mouseover", setupHoverAlert);
+}
 
 
   // Appelle cette fonction une fois au chargement pour activer les survols
-  setupHoverAlert();
-  
+  window.onload = setupHoverAlert(); 
+   
