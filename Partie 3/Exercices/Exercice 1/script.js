1a29830c05448cbb2bcbd07d1867a64586005e55
dinel <rddinel@gmail.com>
2025-05-19 14:24:22 +0200

    Partie 3 - Cours et exercices

---
+++ Partie 3/Exercices/Exercice 1/script.js
@@ -0,0 +1,7 @@
+// On imagine un jeu avec 3 personnages : Un guerrier, une fée et un magicien. 
+// Créez trois classees pour chacun de ces personnages, avec au moins 3 attributs (point de vie, point d'attaque...) et 3 méthodes.
+// On pourra créer des méthodes ou attributs identiques, mais chaque personnage avoir certains attributs et 
+// méthodes spécifiques 
+
+// Dans un second temps, créez trois instances de ces classes, et faites les intéragir en fonction des méthodes 
+// et attributs spécifiques de chaque personnage.
